07193185b6610ba91b71f924d2e27cfe9938f2eb
michaelm602 <48140179+michaelm602@users.noreply.github.com>
2025-07-23 01:03:50 -0700

    hashrouter added

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import ProtectedRoute from "./Components/ProtectedRoute"
 import Home from './pages/Home';
 import Portfolio from "./pages/Portfolio";
