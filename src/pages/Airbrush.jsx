03b8d9006099f1f22d4a77489750656b331250b9
michaelm602 <48140179+michaelm602@users.noreply.github.com>
2025-08-03 13:46:00 -0700

    Update Airbrush.jsx
    import case correction

--- src/pages/Airbrush.jsx
+++ src/pages/Airbrush.jsx
@@ -1,5 +1,5 @@
 // src/pages/Airbrush.jsx
-import Gallery from '../components/Gallery';
+import Gallery from '../Components/Gallery';
 
 export default function Airbrush() {
   return <Gallery folder="airbrush" label="Airbrush" />;
